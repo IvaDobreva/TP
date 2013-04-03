0783cda97556b02b0a75f91d709c085a64c78607
IvaDobreva <iva95dobreva@gmail.com>
2013-04-03 04:04:05 +0300

    Handeling no connection error

--- StupidDreams/src/com/example/stupiddreams/StupidDreams.java
+++ StupidDreams/src/com/example/stupiddreams/StupidDreams.java
@@ -1,55 +1,114 @@
 package com.example.stupiddreams;
 
-import java.util.List;
 import java.util.Locale;
 
+import com.example.stupiddreams.R;
+
+import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.ListFragment;
-import android.support.v4.app.NavUtils;
+import android.support.v4.net.ConnectivityManagerCompat;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class StupidDreams extends FragmentActivity {
+public class StupidDreams extends FragmentActivity implements
+		ActionBar.TabListener {
 
-	/**
-	 * The {@link android.support.v4.view.PagerAdapter} that will provide
-	 * fragments for each of the sections. We use a
-	 * {@link android.support.v4.app.FragmentPagerAdapter} derivative, which
-	 * will keep every loaded fragment in memory. If this becomes too memory
-	 * intensive, it may be best to switch to a
-	 * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-	 */
 	SectionsPagerAdapter mSectionsPagerAdapter;
 
-	/**
-	 * The {@link ViewPager} that will host the section contents.
-	 */
 	ViewPager mViewPager;
 
+	private boolean connectionAvailable() {
+		boolean connected = false;
+		ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		if (connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE)
+				.getState() == NetworkInfo.State.CONNECTED
+				|| connectivityManager.getNetworkInfo(
+						ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+			connected = true;
+		}
+		return connected;
+	}
+
+	private void alertButton() {
+		AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
+		dlgAlert.setMessage("This App needs internet connection.");
+		dlgAlert.setTitle("No Internet connection");
+		dlgAlert.setPositiveButton("Exit", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				finish();
+				
+			}
+		});
+		dlgAlert.show();
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_stupid_dreams);
 
-		// Create the adapter that will return a fragment for each of the three
-		// primary sections of the app.
-		mSectionsPagerAdapter = new SectionsPagerAdapter(
-				getSupportFragmentManager());
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+				.permitAll().build();
+		StrictMode.setThreadPolicy(policy);
 
-		// Set up the ViewPager with the sections adapter.
-		mViewPager = (ViewPager) findViewById(R.id.pager);
-		mViewPager.setAdapter(mSectionsPagerAdapter);
+		if (connectionAvailable() == false) {
+			alertButton();
+		} else {
 
+			// Set up the action bar.
+			final ActionBar actionBar = getActionBar();
+			actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+			// Create the adapter that will return a fragment for each of the
+			// three
+			// primary sections of the app.
+			mSectionsPagerAdapter = new SectionsPagerAdapter(
+					getSupportFragmentManager());
+
+			// Set up the ViewPager with the sections adapter.
+			mViewPager = (ViewPager) findViewById(R.id.pager);
+			mViewPager.setAdapter(mSectionsPagerAdapter);
+
+			// When swiping between different sections, select the corresponding
+			// tab. We can also use ActionBar.Tab#select() to do this if we have
+			// a reference to the Tab.
+			mViewPager
+					.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+						@Override
+						public void onPageSelected(int position) {
+							actionBar.setSelectedNavigationItem(position);
+						}
+					});
+
+			// For each of the sections in the app, add a tab to the action bar.
+			for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
+				// Create a tab with text corresponding to the page title
+				// defined by
+				// the adapter. Also specify this Activity object, which
+				// implements
+				// the TabListener interface, as the callback (listener) for
+				// when
+				// this tab is selected.
+				actionBar.addTab(actionBar.newTab()
+						.setText(mSectionsPagerAdapter.getPageTitle(i))
+						.setTabListener(this));
+			}
+		}
 	}
 
 	@Override
@@ -57,6 +116,24 @@ public class StupidDreams extends FragmentActivity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.stupid_dreams, menu);
 		return true;
+	}
+
+	@Override
+	public void onTabSelected(ActionBar.Tab tab,
+			FragmentTransaction fragmentTransaction) {
+		// When the given tab is selected, switch to the corresponding page in
+		// the ViewPager.
+		mViewPager.setCurrentItem(tab.getPosition());
+	}
+
+	@Override
+	public void onTabUnselected(ActionBar.Tab tab,
+			FragmentTransaction fragmentTransaction) {
+	}
+
+	@Override
+	public void onTabReselected(ActionBar.Tab tab,
+			FragmentTransaction fragmentTransaction) {
 	}
 
 	/**
@@ -71,44 +148,30 @@ public class StupidDreams extends FragmentActivity {
 
 		@Override
 		public Fragment getItem(int position) {
-			// getItem is called to instantiate the fragment for the given page.
-			// Return a DummySectionFragment (defined as a static inner class
-			// below) with the page number as its lone argument.
-			if(position == 0) {
-				Fragment fragment = new StupidDreamsNews();
-				//Bundle args = new Bundle();
-				//Writes numbers on screen
-				//args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 68);
-				//fragment.setArguments(args);
-				return fragment;
-			} else if(position == 1) {
-				Fragment fragment = new DummySectionFragment();
-				Bundle args = new Bundle();
-				//Writes numbers on screen
-				args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 10);
-				fragment.setArguments(args);
-				return fragment;
-			} else  {
-				Fragment fragment = new DummySectionFragment();
-				Bundle args = new Bundle();
-				//Writes numbers on screen
-				args.putInt("Hello",56);
-				fragment.setArguments(args);
-				return fragment;
+			switch (position) {
+			case 0:
+				Fragment news = new ShowXMLContent();
+				Bundle arg = new Bundle();
+				String link = "http://stupid-dreams.ovo.bg/RSSStupidDreams.xml";
+				arg.putString(null, link);
+				news.setArguments(arg);
+				return news;
+			case 1:
+				Fragment projects = new ShowXMLContent();
+				Bundle arg2 = new Bundle();
+				String link2 = "http://stupid-dreams.ovo.bg/RSSStupidDreamsProjects.xml";
+				arg2.putString(null, link2);
+				projects.setArguments(arg2);
+				return projects;
 			}
-			/*
-			Fragment fragment = new DummySectionFragment();
-			Bundle args = new Bundle();
-			//Writes numbers on screen
-			args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 2);
-			fragment.setArguments(args);
-			return fragment;*/
+			return null;
+
 		}
 
 		@Override
 		public int getCount() {
-			// Show 3 total pages.
-			return 3;
+			// Show 2 total pages.
+			return 2;
 		}
 
 		@Override
@@ -119,39 +182,9 @@ public class StupidDreams extends FragmentActivity {
 				return getString(R.string.news).toUpperCase(l);
 			case 1:
 				return getString(R.string.projects).toUpperCase(l);
-			case 2:
-				return getString(R.string.video).toUpperCase(l);
 			}
 			return null;
 		}
+
 	}
-
-	/**
-	 * A dummy fragment representing a section of the app, but that simply
-	 * displays dummy text.
-	 */
-	public static class DummySectionFragment extends Fragment {
-		/**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 */
-		public static final String ARG_SECTION_NUMBER = "section_number";
-
-		public DummySectionFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(
-					R.layout.fragment_stupid_dreams_news, container, false);
-			TextView dummyTextView = (TextView) rootView
-					.findViewById(R.id.section_label);
-			dummyTextView.setText(Integer.toString(getArguments().getInt(
-					ARG_SECTION_NUMBER)));
-			return rootView;
-		}
-	}
-	
-
 }
