ef73ee7c02ae760fcc659e8526c5b27d607a4035
IvaDobreva <iva95dobreva@gamil.com>
2013-05-05 11:55:22 +0300

    Fragment manager test

--- StupidDreamsTest/src/com/example/stupiddreams/test/StupidDreamsTest.java
+++ StupidDreamsTest/src/com/example/stupiddreams/test/StupidDreamsTest.java
@@ -1,5 +1,8 @@
 package com.example.stupiddreams.test;
 
+import org.powermock.api.easymock.PowerMock;
+import org.powermock.api.easymock.internal.invocationcontrol.EasyMockMethodInvocationControl;
+
 import com.example.stupiddreams.StupidDreams;
 
 import android.app.ActionBar;
@@ -11,25 +14,32 @@ import android.preference.PreferenceFragment;
 import android.support.v4.app.FragmentActivity;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.ActivityUnitTestCase;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class StupidDreamsTest extends ActivityInstrumentationTestCase2<StupidDreams>{
+public class StupidDreamsTest extends
+		ActivityInstrumentationTestCase2<StupidDreams> {
 
 	private int buttonId;
 	private StupidDreams activity;
 	private Bundle bundle;
-	
+
 	public StupidDreamsTest() {
 		super(StupidDreams.class);
 	}
-	
+
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
+		setActivityInitialTouchMode(false);
 		activity = getActivity();
-		
+	}
+
+	public void testFragmentManager() {
+	    FragmentActivity activity = getActivity();
+	    assertNotNull(activity.getSupportFragmentManager());
 	}
 	
-
 	@Override
 	protected void tearDown() throws Exception {
 		super.tearDown();
